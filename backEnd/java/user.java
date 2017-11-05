86fcb45b51d196bdedcaf74a4d9745d67f5edb01
Finn <countcasper@hotmail.com>
2017-11-05 23:10:56 +0000

    added a todo and filled out constructor

--- backEnd/java/user.java
+++ backEnd/java/user.java
@@ -4,9 +4,17 @@ class user{
   private String firstName;
   private String lastName;
   private Date birthday;
+  private Date joinDate;
+  private Pasword uPassword
+  //TODO:Make a date class / find out about in built ones.
   private EmailAdress email;
 
-  public user(String userName, EmailAdress email,String firstName, String lastName, Date birthday, Password password){
+  public user(String userName, EmailAdress email,String firstName, String lastName, Date birthday, Password uPassword){
+    this.userName = userName;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = birthday;
+    this.uPassword = uPassword;
 
   }
 }
