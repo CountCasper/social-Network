72080517372ddac695ec27394d78efeff3d8060f
Finn <countcasper@hotmail.com>
2017-11-05 22:58:10 +0000

    added some html, birthday added user.java which is a class which stores users

---
+++ backEnd/java/user.java
@@ -0,0 +1,12 @@
+
+class user{
+  private String userName;
+  private String firstName;
+  private String lastName;
+  private Date birthday;
+  private EmailAdress email;
+
+  public user(String userName, EmailAdress email,String firstName, String lastName, Date birthday, Password password){
+
+  }
+}
