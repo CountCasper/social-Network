985d286602401593be224b9b3402212bf5b096d7
CountCasper <33357550+CountCasper@users.noreply.github.com>
2017-11-06 01:18:42 +0000

    Just testing (#1)
    * added semi colon
    
    * i guess

--- backEnd/java/user.java
+++ backEnd/java/user.java
@@ -5,9 +5,10 @@ class user{
   private String lastName;
   private Date birthday;
   private Date joinDate;
-  private Pasword uPassword
+  private Pasword uPassword;
   //TODO:Make a date class / find out about in built ones.
   private EmailAdress email;
+  
 
   public user(String userName, EmailAdress email,String firstName, String lastName, Date birthday, Password uPassword){
     this.userName = userName;
